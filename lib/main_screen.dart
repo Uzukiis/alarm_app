9826bf5cff3caa54e1733450689280a5cd1a6952
Uzukiis <natanmakielkowski@gmail.com>
2025-11-03 15:30:14 +0100

    modified:   lib/main_screen.dart

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -2,50 +2,66 @@ import 'package:flutter/material.dart';
 //import 'dart:async';
 //import 'package:alarm/alarm.dart';
 
-
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
-@override
-    Widget build(BuildContext context) {
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final List<int> alarms = [];
+  void _addAlarm() {
+    setState(() {
+      alarms.add(alarms.length + 1);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         shape: OvalBorder(),
-          tooltip: 'Increment',
-          onPressed: (){},
-          child: const Icon(Icons.add, color: Colors.white, size: 28),
-          ),
+        tooltip: 'Increment',
+        onPressed: _addAlarm,
+        child: const Icon(Icons.add, color: Colors.white, size: 28),
+      ),
       body: SafeArea(
         child: Container(
           margin: const EdgeInsets.all(16.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Alarm', style: Theme.of(context).textTheme.headlineMedium),
-              SizedBox(height: 30),
-              Center(
-                child: Column(
-                  children: [
-                    Card(
-                      
+              Text('Alarms', style: Theme.of(context).textTheme.headlineMedium),
+              const SizedBox(height: 30),
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: alarms.length,
+                  itemBuilder: (context, index) {
+                    return Card(
                       child: SwitchListTile(
-                        title: const Text('Alarm 1'),
                         value: true,
-                        onChanged: (bool? value) { },
+                        onChanged: (bool value) {},
+                        activeThumbColor: Colors.deepPurple,
+                        title: Text(
+                          'Alarm ${alarms[index]}',
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                        subtitle: const Text(
+                          '07:00 AM',
+                          style: TextStyle(fontSize: 16),
+                        ),
                       ),
-                    ),
-                  ],
+                    );
+                  },
+                
                 ),
-              )
-              /*ListView.builder(
-                itemCount: 1,
-                itemBuilder: (context, index){
-                  return Card(
-                   child: SwitchListTile(value: true, onChanged:(bool? value) { },),
-                  );
-              })*/
-              ]
               ),
+            ],
+          ),
         ),
       ),
-        );
-}}
+    );
+  }
+}
